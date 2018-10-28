d642e3a3bd473ed06504afe3e40c1337b82ff02e
weixiaocike90 <chenrensheng90@163.com>
2018-10-28 17:21:13 +0800

    创建实体类

---
+++ src/main/java/com/learn/mybatis/chapter1/pojo/Role.java
@@ -0,0 +1,54 @@
+package com.learn.mybatis.chapter1.pojo;
+
+public class Role {
+
+	private Long id;
+	
+	private String roleName;
+	
+	private String note;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the roleName
+	 */
+	public String getRoleName() {
+		return roleName;
+	}
+
+	/**
+	 * @param roleName the roleName to set
+	 */
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	/**
+	 * @return the note
+	 */
+	public String getNote() {
+		return note;
+	}
+
+	/**
+	 * @param note the note to set
+	 */
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
+}
